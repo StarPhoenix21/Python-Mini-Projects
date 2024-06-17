c4ed70c5f335b45aaa5a23342a0fb6326e391fe7
w3ngm0 <waggmo@gmail.com>
2024-06-17 14:25:16 +0800

    Adding new python file words.py

---
+++ Hangman_Game/words.py
@@ -0,0 +1,23 @@
+# words.py
+word_list = [
+    "tiger",
+    "elephant",
+    "python",
+    "bicycle",
+    "giraffe",
+    "kangaroo",
+    "mountain",
+    "ocean",
+    "piano",
+    "rainbow",
+    "satellite",
+    "volcano",
+    "astronaut",
+    "robot",
+    "microscope",
+    "galaxy",
+    "hurricane",
+    "jazz",
+    "computer",
+    "airplane"
+]
