92c0aa98b33099db09e00dca3d86a0d8f96ac4c1
w3ngm0 <waggmo@gmail.com>
2024-06-17 14:13:39 +0800

    using and printing display_hangman function for hangman diagram

--- Hangman_Game/hangman.py
+++ Hangman_Game/hangman.py
@@ -1,5 +1,75 @@
 import random
 
+
+def display_hangman(attempts):
+    stages = [
+        """
+           ------
+           |    |
+           |
+           |
+           |
+           |
+        --------
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |
+           |
+           |
+        --------
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |    |
+           |
+           |
+        --------
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|
+           |
+           |
+        --------
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |
+           |
+        --------
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |   /
+           |
+        --------
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |   / \\
+           |
+        --------
+        """,
+    ]
+    return stages[attempts]
+
+
 def hangman():
     word = random.choice(["tiger", "superman", "thor", "doraemon", "avenger", "water", "stream"])
     validletter = 'abcdefghijklmnopqrstuvwxyz'
@@ -15,7 +85,6 @@ def hangman():
                 main = main + "_" + " "
         if main == word:
             print(main)
-            print("You win!")
             break
         print("Guess the word:", main)
         guess = input().casefold()
@@ -24,71 +93,20 @@ def hangman():
             guessmade = guessmade + guess
         else:
             print("Enter a valid character")
-            guess = input()
+            continue
         if guess not in word:
             turns = turns - 1
-        if turns == 9:
-            print("9 turns left")
-            print("  --------  ")
-        if turns == 8:
-            print("8 turns left")
-            print("  ---------  ")
-            print("      O      ")
-        if turns == 7:
-            print("7 turns left")
-            print("  ---------  ")
-            print("      O      ")
-            print("      |      ")
-        if turns == 6:
-            print("6 turns left")
-            print("  ---------  ")
-            print("      O      ")
-            print("      |      ")
-            print("     /       ")
-        if turns == 5:
-            print("5 turns left")
-            print("  ---------  ")
-            print("      O      ")
-            print("      |      ")
-            print("     / \     ")
-        if turns == 4:
-            print("4 turns left")
-            print("  ---------  ")
-            print("    \ O      ")
-            print("      |      ")
-            print("     / \     ")
-        if turns == 3:
-            print("3 turns left")
-            print("  ---------  ")
-            print("    \ O /    ")
-            print("      |      ")
-            print("     / \     ")
-        if turns == 2:
-            print("2 turns left")
-            print("  ---------  ")
-            print("    \ O /|   ")
-            print("      |      ")
-            print("     / \     ")
-        if turns == 1:
-            print("1 turns left")
-            print("Last breaths counting. Take care!")
-            print("  ---------  ")
-            print("    \ O_|/   ")
-            print("      |      ")
-            print("     / \     ")    
-        
-        if turns == 0:    
-            print("You lose")
-            print("You let a kind man die")
-            print("  ---------  ")
-            print("      O_|    ")
-            print("     /|\     ")
-            print("     / \     ")
+
+        print(display_hangman(10 - turns))
+
+        if turns == 0:
+            print("Game Over! The word was:", word)
             break
+
 
 name = input("Enter your name: ")
 print(f"Welcome {name}")
 print("=====================")
-print("Try to guess it less than 10 attempts")
+print("Try to guess it in less than 10 attempts")
 hangman()
 print()
