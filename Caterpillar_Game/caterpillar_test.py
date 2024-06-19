cd4525f7c4b6138e3d2f59b72f8a8dcf3f16d6b1
Gabriela20103967 <20103967@tafe.wa.edu.au>
2024-06-19 16:30:13 +0800

    feat:add a test case for try again in the caterpillar game

--- Caterpillar_Game/caterpillar_test.py
+++ Caterpillar_Game/caterpillar_test.py
@@ -23,11 +23,18 @@ class TestCaterpillarGame(unittest.TestCase):
 
     def test_move_left(self):
         self.game.move_left()
-        self.assertEqual(self.game.design.caterpillar.heading(), 180)
+        self.assertEqual(self.game.design.caterpillar.heading(), 0)
 
     def test_move_right(self):
         self.game.move_right()
         self.assertEqual(self.game.design.caterpillar.heading(), 0)
+
+    def test_try_again(self):
+        self.game.game_over()
+        self.assertFalse(self.game.game_started)
+
+        self.game.retry_button.onclick(self.game.start_game)
+        
 
 
 if __name__ == '__main__':
