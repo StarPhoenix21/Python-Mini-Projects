86d39745c74dde0ff24a508beb99bb7333bd5f75
franchsli <112600427+franchsli@users.noreply.github.com>
2025-01-04 16:49:32 -0500

    Updated deprecated import statement

--- Download Audio/Download Audio.py
+++ Download Audio/Download Audio.py
@@ -1,6 +1,6 @@
 import os
 import pytube
-from moviepy.editor import *
+from moviepy import AudioFileClip
 
 # Define the YouTube video URL
 youtube_url = "https://www.youtube.com/watch?v=E6eKvji_BoE"
